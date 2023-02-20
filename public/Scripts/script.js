d3d62e8762e83e932e68eaa93c7f6e8f0cbd6a45
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2023-02-20 19:07:57 +0100

    changes

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -1,21 +1,21 @@
 const memberbutton = document.querySelectorAll('.memberbutton')
 const personIcon = document.querySelector('.manicon')
-const test2button = document.querySelector('.test2')
-
-test2button.addEventListener('click', changeheight2)
+let membername = document.querySelector('.your-name')
+let memberlength = document.querySelector('.your-height')
 
 memberbutton.forEach((button) => {
-    button.addEventListener('click', changeheight)
+    button.addEventListener('click', ()=> {
+        const height = button.dataset.height
+        const name = button.dataset.name
+        personIcon.style.height = height/210 * 100 +"%";
+        membername.innerHTML = name
+        memberlength.innerHTML = height + "cm"
+    })
 })
 
-function changeheight() {
-    personIcon.style.height = "50%"
-}
-
 function changeheight2() {
-    personIcon.style.height = "80%"
+    personIcon.style.height = "70%"
 }
-
 
 //CALC//
 // personIcon.style.height = "calc(216 - <% member.length %> * 100" %;
